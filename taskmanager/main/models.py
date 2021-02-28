57261027a898d36a50e1365978909bddf6e06f24
Andrew <anisimov.andrew.hi@gmail.com>
2021-02-28 13:44:40 +0300

    Output tasks ordered by id + condition for output

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -8,4 +8,8 @@ class Task(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
 
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
-
